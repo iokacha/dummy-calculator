182cbf25f4f4234999e16555bd4422ef7bda6e29
OKACHA Ilias <i.okacha@prismamedia.com>
2021-02-20 15:33:19 +0100

    implement different input unit

--- function/calculator.py
+++ function/calculator.py
@@ -1,8 +1,26 @@
 from decimal import Decimal
+import enum
+
+
+# meter is the reference
+class Units(enum.Enum):
+    Kilometers  = 0.001
+    Meters      = 1
+    Decimeters  = 10
+    Centimeters = 100
+    Milimeters  = 1000
+    
+    
+
 class Calculator:
 
     def sum(self, input1, input2, unit="Meters"):
         value1, unit1 = input1.split(" ")
         value2, unit2 = input2.split(" ")
-        result = (Decimal(value1) + Decimal(value2)).normalize()
-        return f"{result} {unit}" 
+        meter_value1 = self.to_meter(Decimal(value1), unit1)
+        meter_value2 = self.to_meter(Decimal(value2), unit2)
+        result = Decimal(meter_value1 + meter_value2).normalize()
+        return f"{result} {unit}"
+    
+    def to_meter(self, value, unit):
+        return value / Units[unit].value
--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -1,11 +1,39 @@
 import pytest
-from function.calculator import Calculator
+from function.calculator import Calculator, Units
 
-def test_calculator_meter():
+def test_calculator_meter_and_integer_only():
     calculator = Calculator()
-    # first test 
     result = calculator.sum(
         "3 Meters", 
         "5 Meters"
     )
     assert result == "8 Meters"
+
+def test_calculator_meter_and_float_values():
+    calculator = Calculator()
+    result = calculator.sum(
+        "3.2 Meters", 
+        "5.7 Meters"
+    )
+    assert result == "8.9 Meters"
+
+def test_calculator_different_input_unit():
+    calculator = Calculator()
+    result = calculator.sum(
+        "3 Decimeters", 
+        "5 Meters",
+    )
+    assert result == "5.3 Meters"
+
+    result = calculator.sum(
+        "3 Milimeters", 
+        "5 Meters",
+    )
+    assert result == "5.003 Meters"
+    
+    result = calculator.sum(
+        "3 Milimeters", 
+        "5 Milimeters",
+    )
+    assert result == "0.008 Meters"
+
